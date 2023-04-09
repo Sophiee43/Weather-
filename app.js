654a10c898672c64498e407dcf7ba1c698c4f38b
Sophiee43 <126475876+Sophiee43@users.noreply.github.com>
2023-04-09 00:11:13 -0700

    updated my code

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ let days=[
 ];
 
 let day =days[date.getDay()];
-return`${day} ${hours}:${minutes}`;
+let heading = document.querySelector("#date");
+heading.innerHTML = `${day},${hours}:${minutes}`;
 }
 
 
@@ -37,8 +38,19 @@ function displayTemperature(response) {
  let description=document.queerySelector("#description");
  description.innerHTML=(response.data.main.description);
  let date=document.querySelector("#date");
+ 
+  let currentCity = response.data.name;
+  let Cities = document.querySelector("h1");
+  Cities.innerHTML = `${currentCity}`;
 }
+function searchCity(event) {
+ event.preventDefault();
+ let searchInput = document.querySelector("#city-input");
+   let city = `${searchInput.value}`;
 let apiKey = "2daf65f0cdaa917f11026e8a128ce271";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
 axios.get(url).then(displayTemperature)
+}
+let searchBar = document.querySelector("#search-form");
+searchBar.addEventListener("submit", searchCity);
