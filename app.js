4d398463f8fd2256ae9678850e5642256ff00e44
Sophiee43 <126475876+Sophiee43@users.noreply.github.com>
2023-04-09 10:26:40 -0700

    showed icon

--- app.js
+++ app.js
@@ -27,30 +27,27 @@ heading.innerHTML = `${day},${hours}:${minutes}`;
 
 function displayTemperature(response) {
  console.log(response.data);
- let temperature=document.querySelector("#temperature");
- temperature.innerHTML=Math.round(response.data.main.temp);
- let city=document.querySelector("#city");
- city.innerHTML=respond.data.name;
- let wind=document.querySelector("#wind");
- wind.innerHTML=Math.round(response.data.wind.speed);
+ let temperatureElement=document.querySelector("#temperature");
+ temperatureElement.innerHTML=Math.round(response.data.main.temp);
+ let cityElement=document.querySelector("#city");
+ cityElement.innerHTML=(response.data.name);
+ let windElement=document.querySelector("#wind");
+ windElement.innerHTML=Math.round(response.data.wind.speed);
  let humidity=document.querySelector("#humidity");
  humidity.innerHTML=(response.data.main.humidity);
- let description=document.queerySelector("#description");
- description.innerHTML=(response.data.main.description);
+ let description=document.querySelector("#description");
+ description.innerHTML=(response.data.weather[0].description);
  let date=document.querySelector("#date");
- 
-  let currentCity = response.data.name;
-  let Cities = document.querySelector("h1");
-  Cities.innerHTML = `${currentCity}`;
+ date.innerHTML=formatDate(response.data.dt * 1000);
+ let icon=document.querySelector("#icon");
+ icon.setAttribute("src",` https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+ icon.setAttribute("alt",response.data.weather[0].description)
 }
-function searchCity(event) {
- event.preventDefault();
- let searchInput = document.querySelector("#city-input");
-   let city = `${searchInput.value}`;
-let apiKey = "2daf65f0cdaa917f11026e8a128ce271";
-let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+   let city = `Ikorodu`;
+   let units = "metric";
+  let apiKey = "2daf65f0cdaa917f11026e8a128ce271";
+  let url = "https://api.openweathermap.org/data/2.5/weather?q=";
+  let apiUrl = `${url}${city}&units=${units}&appid=${apiKey}`;
   
-axios.get(url).then(displayTemperature)
-}
-let searchBar = document.querySelector("#search-form");
-searchBar.addEventListener("submit", searchCity);
+axios.get(apiUrl).then(displayTemperature)
+
